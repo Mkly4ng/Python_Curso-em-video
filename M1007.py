2613b6f86453806be8d8537c61cf611c0c2dfc93
Mikhael Yang <165682712+Mkly4ng@users.noreply.github.com>
2025-05-16 08:56:03 -0300

    Create M1007.py

---
+++ M1007.py
@@ -0,0 +1,5 @@
+# a média entre dois alunos
+n1 = int(input('Nota do primeiro aluno: '))
+n2 = int(input('Nota do segundo aluno: '))
+m = (n1+n2)/2
+print('A média das notas dos dois alunos é :{0}' .format(m))
