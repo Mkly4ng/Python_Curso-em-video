3b92379de3e58dab0558c07eeed3282870f74430
Mikhael Yang <165682712+Mkly4ng@users.noreply.github.com>
2025-05-16 10:47:53 -0300

    Create M1D008.py

---
+++ M1D008.py
@@ -0,0 +1,5 @@
+# conversor de metros para centímetros e milímetros
+m = int(input('Insira o valor a ser convertido de metros para centimetros e milimetros: '))
+c = m*100
+ml = m*1000
+print('{0} metros é igual a {1} centimetros e {2} milimetros' .format(m, c, ml))
