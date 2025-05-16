290e1c606d4d8257d32e967b1f4bfeb7d9f1a4aa
Mikhael Yang <165682712+Mkly4ng@users.noreply.github.com>
2025-05-16 10:55:27 -0300

    Create M1D016.py

---
+++ M1D016.py
@@ -0,0 +1,9 @@
+# Pegar a parte inteira de um numero
+from math import floor, trunc
+n = float(input('Digite um número: '))
+#print(floor(n))
+#o floor arredonda pra baixo
+print(trunc(n))
+
+#n = float(input('Digite um valor: ')
+#print('O valor digirado foi {0} e a sua porcao inteira é {1}'.format(n, int(n)))
