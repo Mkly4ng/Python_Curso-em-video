afba816c055bad8b0b5a2de6987152d5fa34c5b2
Mikhael Yang <165682712+Mkly4ng@users.noreply.github.com>
2025-05-16 11:19:39 -0300

    Create M1D024.py

---
+++ M1D024.py
@@ -0,0 +1,8 @@
+# Verificar se o nome da cidade começa com santo
+n = str(input('Escreva o nome de uma cidade: ')).strip()
+m = n.upper()
+s = m.split()
+print('SANTO' in m[0])
+
+'''cid = str(input('Em que cidade voce nasceu? ')).strip()
+print (cid[:5}.upper() == 'SANTO' '''
