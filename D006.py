b74ef0a42d2cdfa3adeb9bb4c89a371ec82d0d37
Mikhael Yang <165682712+Mkly4ng@users.noreply.github.com>
2025-05-16 08:48:22 -0300

    Add files via upload

---
+++ D006.py
@@ -0,0 +1,6 @@
+# Dobro, triplo e raiz quadrada de determinado número
+n = int(input('Escreva um número: '))
+d = n*2
+t = n*3
+r = n**(1/2)
+print('O dobro de {0} é {1}, o triplo de {0} é {2} e a raiz quadrada de {0} é {3}' .format(n, d, t ,r))
