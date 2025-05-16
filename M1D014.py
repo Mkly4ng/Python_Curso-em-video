c126d885433aac8a9bfca2759301e81b70d1f8ae
Mikhael Yang <165682712+Mkly4ng@users.noreply.github.com>
2025-05-16 10:54:03 -0300

    Create M1D014.py

---
+++ M1D014.py
@@ -0,0 +1,4 @@
+# Conversor de Celcius para Fahrenheit
+c = float(input('Informe a temperatura em Celsius: '))
+f = (c * 9 / 5) + 32
+print('A temperatura de {0:.1f} Celsius é equivalente a {1:.1f} Fahrenheit'.format(c, f))
