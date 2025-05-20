f8a0d5f59933910734e2ad39a54ccb854ea81d1f
Mikhael Yang <165682712+Mkly4ng@users.noreply.github.com>
2025-05-20 18:33:06 -0300

    Create M3D114.py

---
+++ M3D114.py
@@ -0,0 +1,10 @@
+# Testar a conexão de um site:
+import urllib
+import urllib.request
+try:
+    site = urllib.request.urlopen('https://www.pudim.com.br/')
+except urllib.error.URLError:
+    print('O site Pudim não está acessível no momento.')
+else:
+    print('Consegui acessar o site Pudim com sucesso!')
+    print(site.read())
