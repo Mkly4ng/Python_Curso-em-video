302efb272be5576156655c83be3306a11723d73a
Mikhael Yang <165682712+Mkly4ng@users.noreply.github.com>
2025-05-20 18:34:50 -0300

    Create M1D004.py

---
+++ M1D004.py
@@ -0,0 +1,21 @@
+frase = 'Curso em Video Python'
+print(frase)
+print(frase.count('o'))
+print(frase[0:22])
+print(len(frase))
+print(frase.find('o'))
+print('Curso'in frase)
+print(frase.replace('Python','Android'))
+print(frase.upper())
+print(frase.lower())
+print(frase.capitalize())
+print(frase.title())
+print(frase.split())
+lista = ['Curso', 'em', 'Vídeo']
+print(' '.join(lista))
+print('''bom dia, meu nome é mikhael
+tenho 20 anos e to aprendendo python
+to fazendo cursinho pra ser engenheiro da computacao''')
+print(frase.upper().count('O'))
+dividido = frase.split()
+print(dividido[2][:])
