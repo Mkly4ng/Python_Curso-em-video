5b23a8995bc38ddb805d8d9283f94778703fd139
Mikhael Yang <165682712+Mkly4ng@users.noreply.github.com>
2025-05-20 18:34:24 -0300

    Create M1D002.py

---
+++ M1D002.py
@@ -0,0 +1,12 @@
+# nome = input('Qual é o seu nome?')
+# print('Prazer em te conhecer {:20}!'.format(nome))
+n1 = int(input('um número '))
+n2 = int(input('outro número '))
+s = n1 + n2
+d = n1 - n2
+div = n1 / n2
+dsr = n1 // n2
+r = n1 % n2
+p = n1 ** n2
+m = n1 * n2
+print('a soma é {}, a subtracao é {}, a divisao é {}, a multiplicacao é {} e a potencia é {}' .format(s, d, div, m, p))
